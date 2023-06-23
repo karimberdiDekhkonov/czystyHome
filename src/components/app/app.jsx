5b4f518e8f6209e014b8890bd4c5722308f29606
Dilrux2007 <urazaliyevdilrux@gmail.com>
2023-06-23 20:04:56 +0500

    admin add

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,10 @@
-import { Navbar, Regular } from "..";
+import { Navbar, Regular,  } from "..";
 import { Box } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import Section5 from "../section5/Section5";
 import Footer from "../Footer/Footer";
+import Renovation from "../renovation/regular";
+import Window from "../window/regular";
 
 
 const App = () => {
@@ -11,7 +13,8 @@ const App = () => {
       <Navbar />
       <Routes>
         <Route path="/regular" element={<Regular />} />
-        <Route path="/admin" element={<Regular />} />
+        <Route path="/renovation" element={<Renovation />} />
+        <Route path="/window" element={<Window />} />
       </Routes>
       <Section5 />
       <Footer/>
